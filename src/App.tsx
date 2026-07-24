9a4b0a2bc200b14c7d7be869dd4006ff30d8a7b0
concertypin <55056558+concertypin@users.noreply.github.com>
2026-07-24 20:47:08 +0900

    chore: simplify header, remove redundant nav

--- src/App.tsx
+++ src/App.tsx
@@ -9,25 +9,15 @@ interface AppProps {
 export default function App({ children }: AppProps) {
     return (
         <div className="min-h-screen bg-base-200 text-base-content">
-            <header className="sticky top-0 z-20 border-b border-base-300 bg-base-100">
+            <header className="border-b border-base-300 bg-base-100">
                 <div className="navbar">
                     <div className="navbar-start">
                         <Link className="text-xl font-bold" href="/dashboard">
                             메일렌즈
                         </Link>
-                        <span className="ml-4 hidden text-sm text-base-content/60 sm:inline">
+                        <span className="ml-4 text-sm text-base-content/60">
                             메일 홍보 검토함
                         </span>
-                    </div>
-                    <div className="navbar-center hidden lg:flex">
-                        <ul className="menu menu-horizontal gap-1 px-1 text-sm">
-                            <li>
-                                <Link href="/dashboard">대시보드</Link>
-                            </li>
-                            <li>
-                                <Link href="/inbox">메일함</Link>
-                            </li>
-                        </ul>
                     </div>
                     <div className="navbar-end">
                         <ThemeToggle />
